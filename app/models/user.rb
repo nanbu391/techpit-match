22ae72ea9ab5ec85ca95192fb91c9ce9920e215a
nanbu391 <nabe48665@gmail.com>
2020-06-16 11:47:08 +0900

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+
+   mount_uploader :img_name, ImgNameUploader
+
+   enum sex: { 男: 0, 女: 1 }
+ end
+
